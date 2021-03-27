7ad5dd73d2916b21080afb35a9e3698c88afec60
jibranabsarulislam <jga26@njit.edu>
2021-03-27 01:09:23 -0400

    changed object handling to arrays and documented main

--- app/src/main/java/main_package/Main.java
+++ app/src/main/java/main_package/Main.java
@@ -17,49 +17,55 @@ import java.util.concurrent.ExecutionException;
 
 public class Main extends AppCompatActivity {
 
-    private final String TAG = "Main.java";
-    private EditText input_summonerName;
-    private Button btn_submit;
-    private SummonerObject currentSummoner;
-
-
+    private final String TAG = "Main.java";                         // tag for Log
+    private EditText input_summonerName, input_secondSummonerName;  // text inputs
+    private Button btn_submit;                                      // submit button
+    private SummonerObject firSummoner, secSummoner;                // two 'friends' summoners (as objects)
+    private SummonerObject[] duo = new SummonerObject[2];           // array of size 2 to store summoners for easy access
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m);
 
-        input_summonerName = findViewById(R.id.input_summonerName);
+        input_summonerName = findViewById(R.id.input_summonerName);             // reference variables to xml elements
+        input_secondSummonerName = findViewById(R.id.input_secondSummonerName);
         btn_submit = findViewById(R.id.btn_submit);
 
-        btn_submit.setOnClickListener(v -> { //submit button
-            // TODO:
-            //
-            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show();
+        btn_submit.setOnClickListener(v -> {                                    //submit button
+//            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show();
             try {
-                currentSummoner = new setSummoner().execute().get();
-            } catch (ExecutionException e) {
+                duo = new setSummoners().execute().get();                       // runs setSummoners class below
+            } catch (ExecutionException e) {                                    // required catch block
                 e.printStackTrace();
-            } catch (InterruptedException e) {
+            } catch (InterruptedException e) {                                  // required catch block
                 e.printStackTrace();
             }
-            Log.i(TAG, "Got summoner name " + currentSummoner.getName());
+            Log.i(TAG, "Summoners: " +                                    // log summoners
+                    duo[0].getName() + ", " +
+                    duo[1].getName());
+//            Toast.makeText(this, "Got Summoners: "  +
+//                    duo[0].getName() + ", " +
+//                    duo[1].getName(), Toast.LENGTH_SHORT).show();
         });
     }
 
-    private class setSummoner extends AsyncTask<Void, Void, SummonerObject> {
+    private class setSummoners extends AsyncTask<Void, Void, SummonerObject[]> {
         @Override
-        protected SummonerObject doInBackground(Void... voids) {
+
+        protected SummonerObject[] doInBackground(Void... voids) {
             try {
-                currentSummoner = new SummonerObject(input_summonerName.getText().toString());
+                firSummoner = new SummonerObject(input_summonerName.getText().toString());          // grab users from input
+                secSummoner = new SummonerObject(input_secondSummonerName.getText().toString());    // TODO: add handling for no inputs
+                duo[0] = firSummoner;  duo[1] = secSummoner;                                        // set users into array
             } catch (RiotApiException e) {
                 Log.e(TAG, "Encountered an error...");
                 e.printStackTrace();
             }
-            return currentSummoner;
+            return duo;                                                                             // return array
         }
         @Override
-        protected void onPostExecute(SummonerObject aVoid) {
-            Log.i(TAG, "Summoner " + currentSummoner.getName() + " grabbed!");
+        protected void onPostExecute(SummonerObject[] aVoid) {
+            Log.i(TAG, "Summoner " + duo[0].getName() + " and " + duo[1].getName() + " grabbed!");
             super.onPostExecute(aVoid);
         }
     }
